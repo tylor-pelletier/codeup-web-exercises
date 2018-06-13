266addf9f161e45d4666b4948fb80b35a3cce746
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-06-13 10:18:05 -0500

    added updates to external.js, renamed css/site.css to correct name css/style.css and updated that name to welcome.html

--- js/external.js
+++ js/external.js
@@ -1,51 +1,50 @@
 "use strict";
 
-// EXERCISE NUMBER 1
+// EXERCISE NUMBER 1:
 
 console.log("Hello from external JavaScript");
 
 alert("Welcome to my website!");
 
-// EXERCISE NUMBER 2
+// EXERCISE NUMBER 2:
 
 var userInput = prompt("What's your favorite color?");
 
-alert("Great," + " " + userInput + " " + "is my favorite color too!");
+alert("Great, " + userInput + " is my favorite color too!");
 
-// EXERCISE NUMBER 3
+// EXERCISE NUMBER 3:
 
 // 1
 
-var pricePerDay = 3;
-
+var pricePerDay = prompt("How much is the daily rental per movie?");
 var littleMermaidDays = prompt("How many days do you want to rent The Little Mermaid?");
 var brotherBearDays = prompt("How many days do you want to rent Brother Bear?");
 var herculesDays = prompt("How many days do you want to rent Hercules?");
-
-alert("You will pay" + " " + "$" + ((parseInt(littleMermaidDays) + parseInt(brotherBearDays) + parseInt(herculesDays)) * pricePerDay));
+var totalCost = (parseInt(littleMermaidDays) + parseInt(brotherBearDays) + parseInt(herculesDays)) * pricePerDay;
+alert("You will pay " + "$" + totalCost);
 
 prompt("You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay? (littleMermaidDays + brotherBearDays + herculesDays) * pricePerDay = $9 + $15 + $3 =");
-
-alert("You will have to pay" + " " + "$" + ((parseInt(littleMermaidDays) + parseInt(brotherBearDays) + parseInt(herculesDays)) * parseInt(pricePerDay)) + ".");
+alert("You will have to pay " + "$" + totalCost + ".");
 
 // 2
 
-var googleRate = 400;
+var googleRate = prompt("How much do you make per hour at Goodle?");
 var googleHours = prompt("How many hours did you work at Google?");
 
-var amazonRate = 380;
+var amazonRate = prompt("How much do you make per hour at Amazon?");
 var amazonHours = prompt("How many hours did you work at Amazon?");
 
-var facebookRate = 350;
+var facebookRate = prompt("How much do you make per hour at Facebook?");
 var facebookHours = prompt("How many hours did you work at Facebook?");
 
-alert("You will receive" + " " + "$" + ((facebookRate * facebookHours) + (googleRate * googleHours) + (amazonRate * amazonHours)) + " " + "for this week.")
+var totalPayment = facebookHours * facebookRate;
+    totalPayment += googleHours * googleRate;
+    totalPayment += amazonHours * amazonRate;
 
-var totalPayment;
+alert("You will receive" + "$" + totalPayment + " for this week.");
 
 prompt("Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon. How much will you receive in payment for this week? ((facebookRate * facebookHours) + (googleRate * googleHours) + (amazonRate * amazonHours)) = $3,500 + $$2,400 + $1,520 =");
-
-alert("You will receive" + " " + "$" + ((facebookRate * facebookHours) + (googleRate * googleHours) + (amazonRate * amazonHours)) + " " + "this week.");
+alert("You will receive " + "$" + totalPayment + " this week.");
 
 // ((facebookRate * facebookHours) + (googleRate * googleHours) + (amazonRate * amazonHours));
 // THESE ARE THE SAME
@@ -55,10 +54,9 @@ alert("You will receive" + " " + "$" + ((facebookRate * facebookHours) + (google
 
 // 3
 
-var classIsFull; // boolean
-var classScheduleWorks; //boolean
-
-var enrolledInClass = classIsFull && classScheduleWorks;
+// var classIsFull; // boolean
+// var classScheduleWorks; //boolean
+// var enrolledInClass = classIsFull && classScheduleWorks;
 
 var classFull = confirm("Is the class open?");
 var classSchedule = confirm("Is your schedule free?");
@@ -74,11 +72,10 @@ else if (enrollStatus === false) {
 
 // 4
 
-var itemsForDiscount; //boolean
-var discountStillActive; //boolean
-var premiumAccount; //boolean
-
-var discount = itemsForDiscount && discountStillActive || premiumAccount;
+// var itemsForDiscount; //boolean
+// var discountStillActive; //boolean
+// var premiumAccount; //boolean
+// var discount = itemsForDiscount && discountStillActive || premiumAccount;
 
 var overTwoItemsSold = confirm("Do they have over two items for purchase?");
 var offerStillGood = confirm("Is the offer still good?");
@@ -88,10 +85,11 @@ console.log(discountStatus);
 
 if (discountStatus === true) {
     alert("Discount applied!")
-}
-else if (discountStatus === false) {
+} else if (discountStatus === false) {
     alert("No discount.")
 }
+//THIS IS ANOTHER WAY OF WRITING IF, ELSE:
+//var discountStatus = (true) ? alert("Discount applied!") : alert("No discount.");
 
 // 5
 
